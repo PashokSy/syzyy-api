608b0bfb84de047ef7412bca239542ccddb77d2b
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-08-17 20:42:05 +0300

    feat: mongoDB
    - add connection to db file;
    - add check for db connection in app.js;
    - move connection check and server start to a separate function

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 import express from 'express';
+import 'dotenv/config';
+
+import { connectDB } from './db/mongoConnection.js';
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Syzyy landing page!');
+  res.send('Syzyy API main endpoint!');
 });
 
-const port = 5000;
-app.listen(port, () => {
-  console.log(`API listening to port ${port}...`);
-});
+// server start
+const start = async () => {
+  try {
+    const dbConnection = await connectDB(process.env.MONGO_URI);
+    console.log(
+      `Connected to DB on '${dbConnection.connection.name}' collection...`
+    );
+
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+      console.log(`API listening to port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
