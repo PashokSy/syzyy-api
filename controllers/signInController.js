82f0a52881e9996b8bb815a2383762d745eb4e5e
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-09-04 21:59:05 +0300

    feat: signInController errors
    add custom errors handling in sign in controller

--- controllers/signInController.js
+++ controllers/signInController.js
@@ -1,15 +1,26 @@
 import bcrypt from 'bcryptjs';
 import { UserModel } from '../models/user.js';
 import { generateJWEToken } from '../utils/jweTokenHelper.js';
+import BadRequestError from '../errors/badRequestError.js';
+import ForbiddenError from '../errors/forbiddenError.js';
+import NotFoundError from '../errors/notFoundError.js';
 
 export const signIn = async (req, res) => {
   const { email, password } = req.body;
 
+  if (!email || !password) {
+    throw new BadRequestError('Please enter credentials');
+  }
+
   try {
     const foundUser = await UserModel.findOne({ email });
 
+    if (!foundUser) {
+      throw new NotFoundError(`User with email '${email}' not found`);
+    }
+
     if (!(await bcrypt.compare(password, foundUser.password)))
-      throw new Error('Wrong password');
+      throw new ForbiddenError('Wrong password');
 
     const jweToken = await generateJWEToken(foundUser);
 
