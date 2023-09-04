6060a9937d0f39eee0bc4bebe7e3cba981c624ad
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-09-04 21:55:51 +0300

    feat: signUpController errors
    add custom errors handling in sign up controller

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -1,10 +1,19 @@
 import { generateJWEToken } from '../utils/jweTokenHelper.js';
 import { UserModel } from '../models/user.js';
+import ConflictError from '../errors/conflictError.js';
+import BadRequestError from '../errors/badRequestError.js';
 
 export const signUp = async (req, res) => {
   const { email, password, firstName, lastName, nickName } = req.body;
 
+  if (!email || !password) {
+    throw new BadRequestError('Please enter credentials');
+  }
+
   try {
+    if (await UserModel.findOne({ email }))
+      throw new ConflictError(`User with email '${email}' already exist`);
+
     const user = await UserModel.create({
       email,
       password,
