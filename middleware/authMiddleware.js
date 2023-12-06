011e50b6f4ff03f09325e2066b5889fb4176b618
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-12-06 21:27:06 +0200

    feat: auth
    - create auth middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,33 @@
+import UnauthorizedError from '../errors/unauthorizedError.js';
+import { decryptJWEToken } from '../utils/jweTokenHelper.js';
+
+export const authenticationMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new UnauthorizedError('No token provided');
+  }
+
+  const tokenArr = authHeader.split(' ');
+
+  if (
+    tokenArr.length !== 2 ||
+    tokenArr.at(0) !== 'Bearer' ||
+    tokenArr.at(1).length === 0
+  ) {
+    throw new UnauthorizedError('Invalid token provided');
+  }
+
+  const token = tokenArr.at(1);
+
+  try {
+    const decoded = await decryptJWEToken(token);
+
+    req.user = JSON.parse(decoded);
+
+    next();
+  } catch (error) {
+    console.error(error);
+    throw new UnauthenticatedError('Not authorized to access this route');
+  }
+};
